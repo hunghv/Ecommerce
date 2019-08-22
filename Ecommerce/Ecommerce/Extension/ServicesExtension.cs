c2acfa19dd2494142e1ad2dd6e7d75bbb8335e06
hoang lucy <hunghvhpu@gmail.com>
2019-08-22 23:10:03 +0700

    init project

---
+++ Ecommerce/Ecommerce/Extension/ServicesExtension.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.OpenApi.Models;
+using System;
+
+namespace Ecommerce.Extension
+{
+    public static class ServicesExtension 
+    {
+        public  static void AddSwagger(this IServiceCollection service)
+        {
+            service.AddSwaggerGen(c =>
+             {
+                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "My API", Version = "v1" });
+             });
+        }
+    }
+}
