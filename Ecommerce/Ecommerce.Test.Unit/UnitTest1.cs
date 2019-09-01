e0703f29e2aed7149fa7a76524aef43032f1ee3f
hung.hoang <hung.hoang@reedonline.co.uk>
2019-09-01 23:35:56 +0700

    Init Project test using Nunit

---
+++ Ecommerce/Ecommerce.Test.Unit/UnitTest1.cs
@@ -0,0 +1,19 @@
+using NUnit.Framework;
+
+namespace Ecommerce.Test.Unit
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
